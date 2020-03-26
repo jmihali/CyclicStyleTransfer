36dfca8e1bb78dbf3ae5266891f9955eb77b6a20
Joan Mihali <jmihali1997@gmail.com>
2020-03-26 18:13:22 +0100

    helpers.py added, msssim loss used for similarity

--- CyclicStyleTransfer.py
+++ CyclicStyleTransfer.py
@@ -1,111 +1,22 @@
 import os
 image_dir = os.getcwd() + '/Images/'
 model_dir = os.getcwd() + '/Models/'
-
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
-import torch.nn.functional as F
 from torch import optim
-
-from torchvision import transforms
-
 from PIL import Image
+from helpers import *
+import pytorch_msssim
 
 #from matplotlib.pyplot import imshow, show, gcf
 
 # In[3]:
 
 img_size = 512
-max_iter = 300
+max_iter = 200
 show_iter = 50
 save_iter = 50
-
-
-#vgg definition that conveniently let's you grab the outputs from any layer
-class VGG(nn.Module):
-    def __init__(self, pool='max'):
-        super(VGG, self).__init__()
-        #vgg modules
-        self.conv1_1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
-        self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.conv2_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.conv3_1 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.conv3_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.conv3_3 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.conv3_4 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.conv4_1 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
-        self.conv4_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv4_3 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv4_4 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv5_1 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv5_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv5_3 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv5_4 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        if pool == 'max':
-            self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-            self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-            self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-            self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
-            self.pool5 = nn.MaxPool2d(kernel_size=2, stride=2)
-        elif pool == 'avg':
-            self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
-            self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
-            self.pool3 = nn.AvgPool2d(kernel_size=2, stride=2)
-            self.pool4 = nn.AvgPool2d(kernel_size=2, stride=2)
-            self.pool5 = nn.AvgPool2d(kernel_size=2, stride=2)
-            
-    def forward(self, x, out_keys):
-        out = {}
-        out['r11'] = F.relu(self.conv1_1(x))
-        out['r12'] = F.relu(self.conv1_2(out['r11']))
-        out['p1'] = self.pool1(out['r12'])
-        out['r21'] = F.relu(self.conv2_1(out['p1']))
-        out['r22'] = F.relu(self.conv2_2(out['r21']))
-        out['p2'] = self.pool2(out['r22'])
-        out['r31'] = F.relu(self.conv3_1(out['p2']))
-        out['r32'] = F.relu(self.conv3_2(out['r31']))
-        out['r33'] = F.relu(self.conv3_3(out['r32']))
-        out['r34'] = F.relu(self.conv3_4(out['r33']))
-        out['p3'] = self.pool3(out['r34'])
-        out['r41'] = F.relu(self.conv4_1(out['p3']))
-        out['r42'] = F.relu(self.conv4_2(out['r41']))
-        out['r43'] = F.relu(self.conv4_3(out['r42']))
-        out['r44'] = F.relu(self.conv4_4(out['r43']))
-        out['p4'] = self.pool4(out['r44'])
-        out['r51'] = F.relu(self.conv5_1(out['p4']))
-        out['r52'] = F.relu(self.conv5_2(out['r51']))
-        out['r53'] = F.relu(self.conv5_3(out['r52']))
-        out['r54'] = F.relu(self.conv5_4(out['r53']))
-        out['p5'] = self.pool5(out['r54'])
-        return [out[key] for key in out_keys]
-
-
-# In[3]:
-
-
-# gram matrix and loss
-class GramMatrix(nn.Module):
-    def forward(self, input):
-        b,c,h,w = input.size()
-        F = input.view(b, c, h*w)
-        G = torch.bmm(F, F.transpose(1,2)) 
-        G.div_(h*w)
-        return G
-
-class GramMSELoss(nn.Module):
-    def forward(self, input, target):
-        """
-        Long version of:
-        f = nn.MSELoss()
-        g = GramMatrix()
-        gm = g(input)
-        out = f(gm, target)
-        """
-        out = nn.MSELoss()(GramMatrix()(input), target)
-        return(out)
-
 
 # In[4]:
 
@@ -114,27 +25,26 @@ class GramMSELoss(nn.Module):
 
 prep = transforms.Compose([transforms.Resize(img_size),
                            transforms.ToTensor(),
-                           transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to BGR
-                           transforms.Normalize(mean=[0.40760392, 0.45795686, 0.48501961], #subtract imagenet mean
-                                                std=[1,1,1]),
+                           transforms.Lambda(lambda x: x[torch.LongTensor([2, 1, 0])]),  # turn to BGR
+                           transforms.Normalize(mean=[0.40760392, 0.45795686, 0.48501961],  # subtract imagenet mean
+                                                std=[1, 1, 1]),
                            transforms.Lambda(lambda x: x.mul_(255)),
-                          ])
-postpa = transforms.Compose([transforms.Lambda(lambda x: x.mul_(1./255)),
-                           transforms.Normalize(mean=[-0.40760392, -0.45795686, -0.48501961], #add imagenet mean
-                                                std=[1,1,1]),
-                           transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to RGB
                            ])
+postpa = transforms.Compose([transforms.Lambda(lambda x: x.mul_(1. / 255)),
+                             transforms.Normalize(mean=[-0.40760392, -0.45795686, -0.48501961],  # add imagenet mean
+                                                  std=[1, 1, 1]),
+                             transforms.Lambda(lambda x: x[torch.LongTensor([2, 1, 0])]),  # turn to RGB
+                             ])
 postpb = transforms.Compose([transforms.ToPILImage()])
-def postp(tensor): # to clip results in the range [0,1]
+
+def postp(tensor):  # to clip results in the range [0,1]
     t = postpa(tensor)
-    t[t>1] = 1    
-    t[t<0] = 0
+    t[t > 1] = 1
+    t[t < 0] = 0
     img = postpb(t)
     return img
 
-
 # In[5]:
-
 
 # get network
 vgg = VGG()
@@ -232,7 +142,9 @@ while n_iter[0] <= max_iter:
         """You take the first loss function, multiply it by its weight, its inputs are A (the image that is going to be optimized)
         and targets[a] (the feature map that we want to specify in our loss)"""
         """Total loss is the sum of the loss of each layer:"""
-        loss = sum(layer_losses) + similarity_weight * nn.MSELoss()(content_image, reversed_img) # added term for similarity loss
+        #loss = sum(layer_losses) + similarity_weight * nn.MSELoss()(content_image, reversed_img) # added term for similarity loss
+        loss = sum(layer_losses) + similarity_weight * pytorch_msssim.msssim(content_image, reversed_img) # added term for similarity loss
+
         loss.backward()
 
         # print loss
@@ -250,7 +162,9 @@ while n_iter[0] <= max_iter:
         """You take the first loss function, multiply it by its weight, its inputs are A (the image that is going to be optimized)
         and targets[a] (the feature map that we want to specify in our loss)"""
         """Total loss is the sum of the loss of each layer:"""
-        loss = sum(layer_losses) + similarity_weight * nn.MSELoss()(content_image, reversed_img) # added term for similarity loss
+        #loss = sum(layer_losses) + similarity_weight * nn.MSELoss()(content_image, reversed_img) # added term for similarity loss
+        loss = sum(layer_losses) + similarity_weight * pytorch_msssim.msssim(content_image, reversed_img) # added term for similarity loss
+
         loss.backward()
 
 
--- NeuralStyleTransfer.py
+++ NeuralStyleTransfer.py
@@ -1,19 +1,13 @@
-import time
-import os 
+import os
 image_dir = os.getcwd() + '/Images/'
 model_dir = os.getcwd() + '/Models/'
 
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
-import torch.nn.functional as F
 from torch import optim
-
-import torchvision
-from torchvision import transforms
-
 from PIL import Image
-from collections import OrderedDict
+from helpers import *
 
 #from matplotlib.pyplot import imshow, show, gcf
 
@@ -21,84 +15,7 @@ from collections import OrderedDict
 
 img_size = 512
 max_iter = 200
-show_iter = 5
-
-#vgg definition that conveniently let's you grab the outputs from any layer
-class VGG(nn.Module):
-    def __init__(self, pool='max'):
-        super(VGG, self).__init__()
-        #vgg modules
-        self.conv1_1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
-        self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.conv2_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.conv3_1 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.conv3_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.conv3_3 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.conv3_4 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.conv4_1 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
-        self.conv4_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv4_3 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv4_4 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv5_1 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv5_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv5_3 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        self.conv5_4 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-        if pool == 'max':
-            self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-            self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-            self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-            self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
-            self.pool5 = nn.MaxPool2d(kernel_size=2, stride=2)
-        elif pool == 'avg':
-            self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
-            self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
-            self.pool3 = nn.AvgPool2d(kernel_size=2, stride=2)
-            self.pool4 = nn.AvgPool2d(kernel_size=2, stride=2)
-            self.pool5 = nn.AvgPool2d(kernel_size=2, stride=2)
-            
-    def forward(self, x, out_keys):
-        out = {}
-        out['r11'] = F.relu(self.conv1_1(x))
-        out['r12'] = F.relu(self.conv1_2(out['r11']))
-        out['p1'] = self.pool1(out['r12'])
-        out['r21'] = F.relu(self.conv2_1(out['p1']))
-        out['r22'] = F.relu(self.conv2_2(out['r21']))
-        out['p2'] = self.pool2(out['r22'])
-        out['r31'] = F.relu(self.conv3_1(out['p2']))
-        out['r32'] = F.relu(self.conv3_2(out['r31']))
-        out['r33'] = F.relu(self.conv3_3(out['r32']))
-        out['r34'] = F.relu(self.conv3_4(out['r33']))
-        out['p3'] = self.pool3(out['r34'])
-        out['r41'] = F.relu(self.conv4_1(out['p3']))
-        out['r42'] = F.relu(self.conv4_2(out['r41']))
-        out['r43'] = F.relu(self.conv4_3(out['r42']))
-        out['r44'] = F.relu(self.conv4_4(out['r43']))
-        out['p4'] = self.pool4(out['r44'])
-        out['r51'] = F.relu(self.conv5_1(out['p4']))
-        out['r52'] = F.relu(self.conv5_2(out['r51']))
-        out['r53'] = F.relu(self.conv5_3(out['r52']))
-        out['r54'] = F.relu(self.conv5_4(out['r53']))
-        out['p5'] = self.pool5(out['r54'])
-        return [out[key] for key in out_keys]
-
-
-# In[3]:
-
-
-# gram matrix and loss
-class GramMatrix(nn.Module):
-    def forward(self, input):
-        b,c,h,w = input.size()
-        F = input.view(b, c, h*w)
-        G = torch.bmm(F, F.transpose(1,2)) 
-        G.div_(h*w)
-        return G
-
-class GramMSELoss(nn.Module):
-    def forward(self, input, target):
-        out = nn.MSELoss()(GramMatrix()(input), target)
-        return(out)
+show_iter = 50
 
 
 # In[4]:
@@ -108,27 +25,26 @@ class GramMSELoss(nn.Module):
 
 prep = transforms.Compose([transforms.Resize(img_size),
                            transforms.ToTensor(),
-                           transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to BGR
-                           transforms.Normalize(mean=[0.40760392, 0.45795686, 0.48501961], #subtract imagenet mean
-                                                std=[1,1,1]),
+                           transforms.Lambda(lambda x: x[torch.LongTensor([2, 1, 0])]),  # turn to BGR
+                           transforms.Normalize(mean=[0.40760392, 0.45795686, 0.48501961],  # subtract imagenet mean
+                                                std=[1, 1, 1]),
                            transforms.Lambda(lambda x: x.mul_(255)),
-                          ])
-postpa = transforms.Compose([transforms.Lambda(lambda x: x.mul_(1./255)),
-                           transforms.Normalize(mean=[-0.40760392, -0.45795686, -0.48501961], #add imagenet mean
-                                                std=[1,1,1]),
-                           transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to RGB
                            ])
+postpa = transforms.Compose([transforms.Lambda(lambda x: x.mul_(1. / 255)),
+                             transforms.Normalize(mean=[-0.40760392, -0.45795686, -0.48501961],  # add imagenet mean
+                                                  std=[1, 1, 1]),
+                             transforms.Lambda(lambda x: x[torch.LongTensor([2, 1, 0])]),  # turn to RGB
+                             ])
 postpb = transforms.Compose([transforms.ToPILImage()])
-def postp(tensor): # to clip results in the range [0,1]
+
+def postp(tensor):  # to clip results in the range [0,1]
     t = postpa(tensor)
-    t[t>1] = 1    
-    t[t<0] = 0
+    t[t > 1] = 1
+    t[t < 0] = 0
     img = postpb(t)
     return img
 
-
 # In[5]:
-
 
 #get network
 vgg = VGG()
@@ -140,7 +56,6 @@ if torch.cuda.is_available():
 
 
 # In[6]:
-
 
 #load images, ordered as [style_image, content_image]
 img_dirs = [image_dir, image_dir]
@@ -169,7 +84,7 @@ for img in imgs:
 
 
 #define layers, loss functions, weights and compute optimization targets
-style_layers = ['r11','r21','r31','r41', 'r51'] 
+style_layers = ['r11', 'r21', 'r31', 'r41', 'r51']
 content_layers = ['r42']
 loss_layers = style_layers + content_layers
 loss_fns = [GramMSELoss()] * len(style_layers) + [nn.MSELoss()] * len(content_layers)
@@ -223,7 +138,7 @@ while n_iter[0] <= max_iter:
     optimizer.step(closure)
     
 #display result
-out_img = postp(opt_img.data.item().cpu().squeeze())
+out_img = postp(opt_img.data[0].cpu().squeeze())
 # imshow(out_img)
 # show()
 out_img.save("Images/nss_out_image.jpg")
